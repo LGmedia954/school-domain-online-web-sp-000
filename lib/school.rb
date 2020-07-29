c8a0c2acf830ba73c680fee284270eb3c4f1e5a3
Leslie G <lesliegonzalez@me.com>
2020-07-29 18:47:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,16 @@ class School
     @roster = {}
   end
   
-  def add_student(student_name, grade)
-    
-    
+  def grade(student_grade)
+    roster[student_grade]
   end
+  
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  
+  
   
 end
 

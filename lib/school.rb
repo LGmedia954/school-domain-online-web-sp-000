68b20ad33358209d89a08f47e115c22bc9fc9096
Leslie G <lesliegonzalez@me.com>
2020-07-29 18:37:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,13 +6,10 @@ class School
   
   def initialize(school_name)
     @school_name = school_name
-    roster = {[]}
-    
-   hash["new_key"] = []
-   hash["new_key"] << "new_value_for_value_array"
- 
-hash
+  end
   
+  def roster
+    roster = {[]}
   end
   
   def add_student(student_name, grade)
